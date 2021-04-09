93935a83809def7d8c7928d72b31a273a44b2b16
Igor Andrade <igorfernandodeandrade@gmail.com>
2021-04-09 17:28:44 -0300

    Eventos para as descrições dos botões foi finalizado

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function clickMeAction (e) {
+let buttonDesc = document.getElementById("button-description");
+
+function clickMeAction () {
 
 	//ACRESCENTAR A LOGICA UTILIZANDO OS SKILL BUTTON
 	++currentAmount;
 	document.getElementById("amount").innerHTML = currentAmount;
+}
+
+// DESCRICAO DO BOTAO 2X
+function show2XDesc () {
+	buttonDesc.innerHTML = "Multiplique por 2 os pontos ganhos por clique";
+}
+
+// DESCRICAO DO BOTAO 10X
+function show10XDesc () {
+	buttonDesc.innerHTML = "Multiplique por 10 os pontos ganhos nos próximos 100 cliques";
+}
+
+// DESCRICAO DO BOTAO MASTER RESET
+function showMasterResetDesc () {
+	buttonDesc.innerHTML = "Zere seus pontos e multiplique por 10 os ganhos por clique";
+}
+
+function hideDesc () {
+	buttonDesc.innerHTML = "";
 }
