17693eb92a8df2b88844500be7d409dba9d154c8
Igor Andrade <igorfernandodeandrade@gmail.com>
2021-04-09 17:07:23 -0300

    Estilização com css finalizada

---
+++ config.js
@@ -0,0 +1,7 @@
+var currentAmount = 0;
+var count2X = 0;
+var countMasterReset = 0;
+var next2XPrice = 0;
+var nextMasterResetPrice = 0;
+var firstMasterResetPrice = 100000;
+var first2XPrice = 100;
---
+++ script.js
@@ -0,0 +1,6 @@
+function clickMeAction (e) {
+
+	//ACRESCENTAR A LOGICA UTILIZANDO OS SKILL BUTTON
+	++currentAmount;
+	document.getElementById("amount").innerHTML = currentAmount;
+}
