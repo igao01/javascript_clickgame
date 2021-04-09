b9dd4f2b3f500f3961ca8d0c145c3ddd2fd025a7
Igor Andrade <igorfernandodeandrade@gmail.com>
2021-04-09 18:48:08 -0300

    Programação do botao 2X iniciada

--- config.js
+++ config.js
@@ -1,7 +1,19 @@
 var currentAmount = 0;
-var count2X = 0;
+var best = 0;
+
+var count2X = 1;
+var first2XPrice = 10;
+var current2XPrice = first2XPrice;
+var next2XPrice = 0; //Math.pow(current2XPrice, 2) - Math.pow(current2XPrice, 2) * 0.6;
+
 var countMasterReset = 0;
-var next2XPrice = 0;
-var nextMasterResetPrice = 0;
-var firstMasterResetPrice = 100000;
-var first2XPrice = 100;
+var firstMasterResetPrice = 10000;
+var currentMasterReset = firstMasterResetPrice;
+var nextMasterResetPrice = 0; //Math.pow(currentMasterReset, 2) - Math.pow(currentMasterReset, 2) * 0.5;
+
+var first10XPrice = 100
+var current10XPrice = first10XPrice;
+var next10XPrice = 0; // Math.pow(current10XPrice, 2) - Math.pow(current10XPrice, 2) * 0.5;
+
+// Prices
+// x * x - (x * x * 0.2)   
--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 let buttonDesc = document.getElementById("button-description");
 
+//CLICK ME ACTION
 function clickMeAction () {
 
 	//ACRESCENTAR A LOGICA UTILIZANDO OS SKILL BUTTON
-	++currentAmount;
+	if (count2X > 1) {
+		currentAmount = currentAmount + count2X;
+		console.log(count2X);
+	}
+	else {
+		++currentAmount;
+	}
+
+
 	document.getElementById("amount").innerHTML = currentAmount;
+}
+
+// 2X ACTION
+function button2XAction () {
+	count2X = count2X * 2;
+	next2XPrice = Math.pow(current2XPrice, 2) - Math.pow(current2XPrice, 2) * 0.6;
+	current2XPrice = next2XPrice;
+}
+
+// 10X ACTION
+function button10XAction () {
+
+}
+
+// MASTER RESET ACTION
+function buttonMasterResetAction () {
+
 }
 
 // DESCRICAO DO BOTAO 2X
