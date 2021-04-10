238be9be04dde66c28668b06de08cd0869436c95
Igor Andrade <igorfernandodeandrade@gmail.com>
2021-04-10 14:34:22 -0300

    Melhoria no CSS e HTML, função button2XAction finalizada

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function clickMeAction () {
 	//ACRESCENTAR A LOGICA UTILIZANDO OS SKILL BUTTON
 	if (count2X > 1) {
 		currentAmount = currentAmount + count2X;
-		console.log(count2X);
+		console.log("ganho por click" + count2X);
 	}
 	else {
 		++currentAmount;
@@ -18,9 +18,18 @@ function clickMeAction () {
 
 // 2X ACTION
 function button2XAction () {
-	count2X = count2X * 2;
-	next2XPrice = Math.pow(current2XPrice, 2) - Math.pow(current2XPrice, 2) * 0.6;
-	current2XPrice = next2XPrice;
+
+	if (currentAmount >= current2XPrice) {
+		currentAmount -= current2XPrice; //subtrai o preco do 2X do valor total
+		count2X = count2X * 2;
+		next2XPrice = Math.pow(current2XPrice, 2) - Math.pow(current2XPrice, 2) * 0.8;
+		console.log("Proximo valor do 2X: " + next2XPrice);
+		current2XPrice = next2XPrice;
+
+	} else {
+		alert("Você precisa de " + current2XPrice + "pontos para utilizar o 2X")
+	}
+
 }
 
 // 10X ACTION
